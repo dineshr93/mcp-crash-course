6fa7450d68d211bd226a5f575754ea30768ffac1
Dinesh <Dinesh.Ravi@elektrobit.com>
2025-05-16 18:02:51 +0200

    Add initial project setup with .gitignore, Python version, main script, and dependencies

---
+++ main.py
@@ -0,0 +1,15 @@
+import asyncio
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+print(OPENAI_API_KEY)
+
+async def main():
+    print("Hello from mcp-crash-course!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
