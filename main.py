f875c29806cd3efbbf9cf1e7986f8e11be36c551
Dinesh <Dinesh.Ravi@elektrobit.com>
2025-05-16 19:29:03 +0200

    Update Python version to 3.12 and enhance main.py with MCP client integration, including session initialization and tool invocation for arithmetic queries.

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import asyncio
 from dotenv import load_dotenv
-import os
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm = ChatOpenAI()
 
-print(OPENAI_API_KEY)
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["D:\\github\\mcps\\lc\\mcp-crash-course\\servers\\math_server.py"],
+)
 
 async def main():
-    print("Hello from mcp-crash-course!")
-
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized")
+            # tools = await session.list_tools()
+            tools = await load_mcp_tools(session)
+            # print(tools)
+            agent = create_react_agent(
+                llm,
+                tools
+            )
+            result = await agent.ainvoke({"messages": "what's (3 + 5) x 12?"})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
